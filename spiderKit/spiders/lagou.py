b4b276b637799b8442d4d5316c674beb4b39a34c
yaochenfeng <282696845@qq.com>
2019-03-31 00:27:31 +0800

    添加自定义设置

--- spiderKit/spiders/lagou.py
+++ spiderKit/spiders/lagou.py
@@ -6,6 +6,9 @@ class LagouSpider(scrapy.Spider):
     name = 'lagou'
     allowed_domains = ['lagou.com']
     start_urls = ['http://lagou.com/']
-
+    # 自定义setting
+    custom_settings = {
+        'SOME_SETTING': 'some value',
+    }
     def parse(self, response):
         pass
